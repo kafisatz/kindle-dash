2a67a53fbcb6e3d6eb31678652309e3767c12c58
Bernhard König <github@diro.ch>
2024-08-27 10:38:09 +0200

    write battery to influxdb and add uptimekuma check

--- src/local/fetch-dashboard.sh
+++ src/local/fetch-dashboard.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env sh
 # Fetch a new dashboard image, make sure to output it to "$1".
 # For example:
-"$(dirname "$0")/../xh" -d -q -o "$1" get https://raw.githubusercontent.com/pascalw/kindle-dash/master/example/example.png
+#"$(dirname "$0")/../xh" -d -q -o "$1" get https://raw.githubusercontent.com/pascalw/kindle-dash/master/example/example.png
+"$(dirname "$0")/../xh" -d -q -o "$1" get https://uptimekumastatus.diro.ch/uptimekumastatus.png
+
+#write battery to influxdb
+#battery level as integer (eg. 77)
+battery_level=$(gasgauge-info -c | sed 's/%//g')
+echo $battery_level
+
+unix_timestamp_now=$(date +%s)
+#e.g. 1724747405
+#note: the token only has access to the kindle bucket and the influx DB is hosted on a LAN
+curl --request POST "https://influx.diro.ch/api/v2/write?org=099e0ad128fe577c&bucket=kindle" --header "Authorization: Token Q0fWs4RcZg2-r85TmPUE1bkH1UP0CL-RjJ3NVIg2C-r-lqCJ9jqcXUlTnBFn9ZsYqtYdwGT0ST1G0tEuNFhIqg==" --header "Content-Type: application/json" --data-binary "battery_level value=${battery_level} ${unix_timestamp_now}000000000"
--- src/local/low-battery.sh
+++ src/local/low-battery.sh
@@ -9,9 +9,9 @@ now=$(date +%s)
 # battery every 24 hours.
 
 if [ "$previous_report_timestamp" -eq -1 ] ||
-  [ $((now - previous_report_timestamp)) -gt 86400 ]; then
+  [ $((now - previous_report_timestamp)) -gt 86400 ]; then  
   # Replace this with for example an HTTP call via curl, or xh
   echo "Reporting low battery: $battery_level_percentage%"
-
+	curl "https://uptimekuma.diro.ch/api/push/lAkNKBATFX?status=up&msg=OK&ping=2"
   echo "$now" >"$last_battery_report_state"
 fi
